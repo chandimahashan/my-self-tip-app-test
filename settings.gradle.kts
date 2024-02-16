cdb9f8587c00cfa51c633720c44c4e7dbe3ab32a
Chandima Hashan <chandima@agrithmics.lk>
2024-02-16 09:36:39 +0530

    my 1st commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "my 1st project tip app"
+include(":app")
+ 
