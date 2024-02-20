003e59f302aa8745feba24c7df4813393afe4c7e
Chandima Hashan <chandima@agrithmics.lk>
2024-02-20 13:42:52 +0530

    my 2st commit for my one the tip app 2/17/2024 3.40 AM

--- app/src/main/java/com/example/my1stprojecttipapp/MainActivity.kt
+++ app/src/main/java/com/example/my1stprojecttipapp/MainActivity.kt
@@ -5,101 +5,94 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
+import android.view.View
+import android.widget.Button
 import android.widget.EditText
-import android.widget.SeekBar
 import android.widget.TextView
-import androidx.core.widget.addTextChangedListener
-
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var youAmountEnter: EditText
-    private lateinit var setTipChange: SeekBar
-    private lateinit var setTip: TextView
     private lateinit var tipAmountYouAdd: TextView
     private lateinit var totalAmountYouPly: TextView
-    private lateinit var enterPercentage:EditText
-    private  var enterAmount = "0"
-    private var enterTip = 0
+    private lateinit var enterPercentage: EditText
+    private lateinit var button1: Button
+    private var enterAmount = "0"
     private var enterPercentageStore = "0"
-
+    private var defaultTip = 1000
     private val tag = "my test"
-    //private val tipProgress = 10
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         youAmountEnter = findViewById(R.id.YouAmountEnter)
-        setTipChange = findViewById(R.id.SetTipChange)
-        setTip = findViewById(R.id.SetTip)
         tipAmountYouAdd = findViewById(R.id.TipAmountYouAdd)
         totalAmountYouPly = findViewById(R.id.TotalAmountYouPly)
         enterPercentage = findViewById(R.id.EnterPercentage)
+
+
+        enterPercentage.setText("5")
 
         youAmountEnter.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
 
             override fun afterTextChanged(p0: Editable?) {
-                if (p0.toString().isNotEmpty()){
+                if (p0.toString().isNotEmpty()) {
                     Log.i(tag, "Enter $p0")
                     enterAmount = p0.toString()
-                    SetTipAndTotalAmount(enterTip)
-
-                }
-                else{
+                    if (defaultTip==1000){
+                        setTipAndTotalAmount(5)
+                    }else {
+                        setTipAndTotalAmount(enterPercentageStore.toInt())
+                    }
+                } else {
                     Log.i(tag, "Enter number $p0")
                     enterAmount = "0"
-                    SetTipAndTotalAmount(enterTip)
-
+                    setTipAndTotalAmount(enterPercentageStore.toInt())
                 }
             }
         })
-     //  setTipChange.progress = enterPercentageStore.toInt()
-//        setTip.text = " Set Tip $tipProgress%"
-         // SetTipAndTotalAmount(enterPercentageStore.toInt())
-        setTipChange.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
-            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
-                Log.i(tag, "Enter Seek bar $p1")
-                val setTipValue = p1.toString()
-                setTip.text = "Set Tip Percentage = $setTipValue%"
-                enterTip = p1
-                SetTipAndTotalAmount(enterTip)
-            }
 
-            override fun onStartTrackingTouch(p0: SeekBar?) {}
-            override fun onStopTrackingTouch(p0: SeekBar?) {}
-        })
-        enterPercentage.addTextChangedListener(object :TextWatcher{
+        enterPercentage.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
 
             override fun afterTextChanged(p0: Editable?) {
+                   defaultTip =2000
+                if (p0.toString().isNotEmpty()) {
+                    Log.i(tag, "enter percentage if $p0")
+                    enterPercentageStore = p0.toString()
+                    setTipAndTotalAmount(enterPercentageStore.toInt())
+                } else {
 
-
-                if (p0.toString().isNotEmpty()){
-                    enterPercentageStore =p0.toString()
-                    setTipChange.progress = enterPercentageStore.toInt()
-                    SetTipAndTotalAmount(enterPercentageStore.toInt())
-
-                }
-                else{
-
-                    setTipChange.progress = 0
-                    SetTipAndTotalAmount(0)
-
+                    Log.i(tag, "enter percentage else $p0")
+                    enterPercentageStore = "0"
+                    setTipAndTotalAmount(0)
                 }
             }
         })
 
-
     }
 
-    private fun SetTipAndTotalAmount(enterTip: Int) {
-        tipAmountYouAdd.text = "${enterAmount.toInt()*enterTip/100}"
-        totalAmountYouPly.text = "${enterAmount.toInt()*enterTip/100+enterAmount.toInt()}"
+    private fun setTipAndTotalAmount(enterPercentageStore: Int) {
+        Log.i(tag, "SetTipAndTotalAmount enterTip $enterPercentageStore")
+        Log.i(tag, "SetTipAndTotalAmount enterAmount $enterAmount")
+        tipAmountYouAdd.text = "${enterAmount.toInt() * enterPercentageStore / 100}"
+        totalAmountYouPly.text = "${enterAmount.toInt() * enterPercentageStore / 100 + enterAmount.toInt()}"
     }
 
+    fun defaultTipButton(view: View) {
+        when(view.id){
+            R.id.button1 -> enterPercentage.setText("10")
+            R.id.button2 -> enterPercentage.setText("15")
+            R.id.button3 -> enterPercentage.setText("20")
+            R.id.button4 -> enterPercentage.setText("25")
+            R.id.button5 -> enterPercentage.setText("30")
+            R.id.button6 -> enterPercentage.setText("35")
+
+        }
+    }
 
 }
